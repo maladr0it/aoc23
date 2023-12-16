2445a3ee53abdec3f0d0c8e48014ae0e82ac3f21
maladr0it <bradford.lachlan@gmail.com>
2023-12-16 15:04:53 -0800

    tidy up

--- src/main.zig
+++ src/main.zig
@@ -5,10 +5,10 @@ const day2 = @import("./day2.zig");
 const day3 = @import("./day3.zig");
 
 pub fn main() !void {
-    // std.debug.print("day1_p1: {!}\n", .{day1.p1()}); // 54940
-    // std.debug.print("day1_p2: {!}\n", .{day1.p2()}); // 54208
-    // std.debug.print("day2_p1: {!}\n", .{day2.p1()}); // 2268
-    // std.debug.print("day2_p2: {!}\n", .{day2.p2()}); // 63542
-    // std.debug.print("day3_p1: {!}\n", .{day3.p1()}); // 527144
-    // std.debug.print("day3_p2: {!}\n", .{day3.p2()}); // 81463996
+    std.debug.print("day1_p1: {!}\n", .{day1.p1()}); // 54940
+    std.debug.print("day1_p2: {!}\n", .{day1.p2()}); // 54208
+    std.debug.print("day2_p1: {!}\n", .{day2.p1()}); // 2268
+    std.debug.print("day2_p2: {!}\n", .{day2.p2()}); // 63542
+    std.debug.print("day3_p1: {!}\n", .{day3.p1()}); // 527144
+    std.debug.print("day3_p2: {!}\n", .{day3.p2()}); // 81463996
 }
